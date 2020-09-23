ae268a7a5d55200a2440023b1bfb6625ef21fd0c
Nur Hakim <noorhakim.s91@gmail.com>
2020-09-23 22:09:34 +0700

    fixing cors config

--- src/main/java/com/rest/assignment/daofab/controller/WebConfiguration.java
+++ src/main/java/com/rest/assignment/daofab/controller/WebConfiguration.java
@@ -1,6 +1,7 @@
 package com.rest.assignment.daofab.controller;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -22,5 +23,10 @@ public class WebConfiguration implements WebMvcConfigurer {
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
     registry
         .addResourceHandler("/docs/**").addResourceLocations("classpath:/META-INF/resources/");
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**");
   }
 }
